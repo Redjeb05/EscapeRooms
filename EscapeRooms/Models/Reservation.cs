752f55ebc3b260bd8706735329b3bff5f5975e97
Redjeb Ali <aliredjeb728@gmail.com>
2024-01-20 00:14:16 +0200

    Fix the problem

--- EscapeRooms/Models/Reservation.cs
+++ EscapeRooms/Models/Reservation.cs
@@ -3,15 +3,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EscapeRooms.Models
 {
-
+    
     public class Reservation
     {
         public int Id { get; set; }
-        public DateTime DateTime { get; set; }
+        public  DateTime DateTime { get; set; }
         public string Description { get; set; }
         public int RoomId { get; set; }
         [ForeignKey(nameof(RoomId))]
-        public Room Room { get; set; }
+        public virtual Room? Room { get; set; }
     }
     }
 
